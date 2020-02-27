28182d564718e2e03ab62dba742c564fe083632b
Alexandru <acazan@outlook.com>
2020-02-27 10:59:49 +0200

    initial

---
+++ README.md
@@ -0,0 +1,1 @@
+# SSB-v4-studio
---
+++ dashboardConfig.js
@@ -0,0 +1,47 @@
+export default {
+  widgets: [
+    { name: 'structure-menu' },
+    {
+      name: 'project-info',
+      options: {
+        __experimental_before: [
+          {
+            name: 'netlify',
+            options: {
+              description:
+                'NOTE: Because these sites are static builds, they need to be re-deployed to see the changes when documents are published.',
+              sites: [
+                {
+                  buildHookId: '5e37eb70cf4258182bde0022',
+                  title: 'Sanity Studio',
+                  name: 'SSB-v4-studio',
+                  apiId: '8a902293-7374-4d4d-b1f8-4df2949c402a'
+                },
+                {
+                  buildHookId: '5e37eb701c77181b42d40fbe',
+                  title: 'Blog Website',
+                  name: 'SSB-v4',
+                  apiId: '099fbaf9-443d-431f-9764-d61681ee57e3'
+                }
+              ]
+            }
+          }
+        ],
+        data: [
+          {
+            title: 'GitHub repo',
+            value: 'https://github.com/naz01101011/SSB-v4',
+            category: 'Code'
+          },
+          { title: 'Frontend', value: 'https://SSB-v4.netlify.com', category: 'apps' }
+        ]
+      }
+    },
+    { name: 'project-users', layout: { height: 'auto' } },
+    {
+      name: 'document-list',
+      options: { title: 'Recent blog posts', order: '_createdAt desc', types: ['post'] },
+      layout: { width: 'medium' }
+    }
+  ]
+}
