ad31e232f509635b404808d34666358b2b5a9a34
Alexandru <acazan@outlook.com>
2020-02-27 11:22:27 +0200

    updated studio with netlify details

--- dashboardConfig.js
+++ dashboardConfig.js
@@ -12,16 +12,10 @@ export default {
                 'NOTE: Because these sites are static builds, they need to be re-deployed to see the changes when documents are published.',
               sites: [
                 {
-                  buildHookId: '5e37eb70cf4258182bde0022',
+                  buildHookId: '5e578a151448fda181a695a2',
                   title: 'Sanity Studio',
-                  name: 'SSB-v4-studio',
-                  apiId: '8a902293-7374-4d4d-b1f8-4df2949c402a'
-                },
-                {
-                  buildHookId: '5e37eb701c77181b42d40fbe',
-                  title: 'Blog Website',
-                  name: 'SSB-v4',
-                  apiId: '099fbaf9-443d-431f-9764-d61681ee57e3'
+                  name: 'SSB Studio',
+                  apiId: '9857e5c0-4089-40d3-8493-1985bf3e7583'
                 }
               ]
             }
