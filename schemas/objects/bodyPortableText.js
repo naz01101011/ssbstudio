2de4a72aa8430e150e6eb2ce7c70e9f8031c0604
Alex Cazan <alex@switchcase.dev>
2021-05-13 15:04:20 +0300

    Added support for embeded youtube videos

--- schemas/objects/bodyPortableText.js
+++ schemas/objects/bodyPortableText.js
@@ -51,6 +51,9 @@ export default {
     },
     {
       type: 'table'
+    },
+    {
+      type: 'youtube'
     }
   ]
 }
--- schemas/schema.js
+++ schemas/schema.js
@@ -17,6 +17,7 @@ import bioPortableText from './objects/bioPortableText'
 import excerptPortableText from './objects/excerptPortableText'
 import mainImage from './objects/mainImage'
 import authorReference from './objects/authorReference'
+import youtube from './objects/youtube'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -36,7 +37,8 @@ export default createSchema({
     bodyPortableText,
     bioPortableText,
     excerptPortableText,
-    banner
+    banner,
+    youtube
 
     // When added to this list, object types can be used as
     // { type: 'typename' } in other document schemas
